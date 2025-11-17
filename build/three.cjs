443d51a016d69f11798bb3bd37e8dd12e417c66b
Meir Arani <myrrh@laughingstock.me>
2025-11-17 09:27:23 +0900

    converting RawUniformsGroups

--- build/three.cjs
+++ build/three.cjs
@@ -30757,30 +30757,29 @@ class UniformsGroup extends EventDispatcher {
  * @author robertlong / https://github.com/robertlong
  */
 
-function RawUniformsGroup(data) {
-	UniformsGroup.call(this);
-	this.data = data;
-	this.autoUpdate = true;
-	this.needsUpdate = false;
-}
-RawUniformsGroup.prototype = Object.assign(Object.create(UniformsGroup.prototype), {
-	constructor: RawUniformsGroup,
-	isRawUniformsGroup: true,
-	add: function (_uniform) {
+class RawUniformsGroup extends UniformsGroup {
+	constructor(data) {
+		super();
+		this.data = data;
+		this.autoUpdate = true;
+		this.needsUpdate = false;
+		this.isRawUniformsGroup = true;
+	}
+	add(uniform) {
 		console.warn('THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.');
 		return this;
-	},
-	remove: function (_uniform) {
-		console.warn('THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.');
+	}
+	remove(uniform) {
+		console.warn('THREE.RawUniformsGroup: .remove() is unimplemented. Modify .data manually instead.');
 		return this;
-	},
-	copy: function (source) {
-		UniformsGroup.prototype.copy.call(this);
+	}
+	copy(source) {
+		super.copy(this);
 		this.autoUpdate = source.autoUpdate;
 		this.data = source.data.slice(0);
 		return this;
 	}
-});
+}
 
 class InstancedInterleavedBuffer extends InterleavedBuffer {
 	constructor(array, stride, meshPerAttribute = 1) {
--- build/three.module.js
+++ build/three.module.js
@@ -47917,50 +47917,38 @@ class UniformsGroup extends EventDispatcher {
  * @author robertlong / https://github.com/robertlong
  */
 
-function RawUniformsGroup( data ) {
 
-	UniformsGroup.call( this );
 
-	this.data = data;
-	this.autoUpdate = true;
-	this.needsUpdate = false;
+class RawUniformsGroup extends UniformsGroup {
+    constructor(data) {
+        super();
+        this.data = data;
+        this.autoUpdate = true;
+	    this.needsUpdate = false;
+        this.isRawUniformsGroup = true;
+    }
 
-}
-
-RawUniformsGroup.prototype = Object.assign( Object.create( UniformsGroup.prototype ), {
-
-	constructor: RawUniformsGroup,
-
-	isRawUniformsGroup: true,
-
-	add: function ( _uniform ) {
-
-		console.warn( 'THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.' );
+    add(uniform) {
+        console.warn( 'THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.' );
 
 		return this;
+    }
 
-	},
-
-	remove: function ( _uniform ) {
-
-		console.warn( 'THREE.RawUniformsGroup: .add() is unimplemented. Modify .data manually instead.' );
+    remove(uniform) {
+        console.warn( 'THREE.RawUniformsGroup: .remove() is unimplemented. Modify .data manually instead.' );
 
 		return this;
+    }
 
-	},
-
-	copy: function ( source ) {
-
-		UniformsGroup.prototype.copy.call( this );
+    copy(source) {
+        super.copy(this);
 
 		this.autoUpdate = source.autoUpdate;
 		this.data = source.data.slice( 0 );
 
 		return this;
-
-	}
-
-} );
+    }
+}
 
 class InstancedInterleavedBuffer extends InterleavedBuffer {
 
